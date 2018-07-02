90562009fcf283297c7a74927dcc6a00078f308d
Yang Chen <yangadam.chen@mail.utoronto.ca>
2018-07-02 12:23:27 -0400

    Added CardHolder attributes and method signatures.

--- phase1/src/CardHolder.java
+++ phase1/src/CardHolder.java
@@ -1,2 +1,30 @@
-public class CardHolder {
+import java.util.ArrayList;
+
+public class CardHolder extends UserAccount {
+
+    private ArrayList<Integer> travelCards;
+    private static int nextAccountNum = 1;
+
+    public CardHolder(String name, String email) {
+        super(name, email);
+        this.accountNumber = nextAccountNum;
+        nextAccountNum += 1;
+        this.travelCards = new ArrayList<>();
+    }
+
+    public void linkCard(Card card) {
+
+    }
+
+    public void deLinkCard(Card card) {
+
+    }
+
+    public void  getMonthlyCost(){
+
+    }
+
+
+
 }
+
