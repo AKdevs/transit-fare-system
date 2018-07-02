864bdcb98f80654d39e5ce7f6b53f68b368c3d3d
Yang Chen <yangadam.chen@mail.utoronto.ca>
2018-07-02 12:12:24 -0400

    Added UserAccount attributes and method signatures.

--- phase1/src/UserAccount.java
+++ phase1/src/UserAccount.java
@@ -1,2 +1,43 @@
 public class UserAccount {
+    private String name;
+    private String email;
+    private int accountNumber;
+
+    public UserAccount(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName(){
+
+    }
+
+    public String getEmail(){
+
+    }
+
+    public String getAccountNum(){
+
+    }
+
+    public String changeName(String newName){
+
+    }
+
+    public String viewBalance(Card card){
+
+    }
+
+    public String activateCard(Card card){
+
+    }
+
+    public String deactivateCard(Card card){
+
+    }
+
+    public void viewInfo(){
+    }
+
+
 }
