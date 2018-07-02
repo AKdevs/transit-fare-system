41ac9059e6be3ffb0cf2bca245d7bf5f387211e1
Yang Chen <yangadam.chen@mail.utoronto.ca>
2018-07-02 12:28:54 -0400

    Added AdminUser attributes and method signatures.

--- phase1/src/AdminUser.java
+++ phase1/src/AdminUser.java
@@ -1,2 +1,18 @@
-public class AdminUser {
+public class AdminUser extends UserAccount {
+
+    private static long nextAccountNum = 888800000000;
+
+    public AdminUser(String name, String email) {
+        super(name, email);
+        this.accountNumber = nextAccountNum;
+        nextAccountNum += 1;
+    }
+
+
+    public void getDailyReport() {
+
+    }
+
+
+
 }
