ff3bab1ee7261f7bf1d1e5b69d1048e1cf3ea74f
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2018-07-02 20:41:30 -0400

    Added constructor, method signatures for TransitLine.

--- phase1/src/TransitLine.java
+++ phase1/src/TransitLine.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class TransitLine {
+    ArrayList<String> points;
+    String type;
+
+    TransitLine(ArrayList<String> points, String type) {
+        this.points = points;
+        this.type = type;
+    }
+
+    ArrayList<String> getPoints() {
+        return this.points;
+    }
+
+    void changeName(String oldName, String newName) {}
+
+    void addPoint() {}
+
 }
