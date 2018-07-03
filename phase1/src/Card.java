d761c9e05019e7d34fa7c7e6d082322c42073058
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-07-03 13:31:24 -0400

    Added necessary attributes and method signatures to Card.

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -1,2 +1,63 @@
+import java.util.ArrayList;
+
 public class Card {
+    private int cardNumber;// static
+    private int balance;
+    private CardHolder owner;
+    private String status;
+    private int currentFare;
+    private ArrayList<ArrayList<tripSegment>> trips; // key-value pair
+    private int totalFares; // key-value pair, past 12 months
+    private int currentTime;
+
+
+    // registered = linked, different from "activate"
+
+    public Card(String cardNumber){
+        this.cardNumber += 1;
+        this.owner = null;
+        this.balance = 19;
+        this.status = "deactivated";
+        this.trips = new ArrayList<ArrayList>(); // only keeps track of 3 most recent trips
+        this.currentFare = 0;
+        this.totalFares = 0;
+        this.currentTime = 0;
+    }
+
+
+    private void addBalance(int value) {
+        this.balance += value;
+    }
+
+    private void deductBalance(int value) {
+        this.balance -= value;
+    }
+
+    private void deactivate(){
+        this.status = "deactivated";
+    }
+
+    private void activate(){
+        this.status = "activated";
+    }
+
+    void viewRecentTrips(){}
+
+    void getCardNumber(){}
+
+    void getBalance(){}
+
+    String getOwner(){}
+
+    String getStatus(){return this.status;}
+
+    int getCurrentFare(){return this.currentFare;}
+
+    private void setCurrentFare(int fare){
+        this.currentFare = fare;
+    }
+
+    void updateTime(int time){}
+
+
 }
