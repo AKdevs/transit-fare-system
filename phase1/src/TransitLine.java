95b0d9901d484b42238d1324670560a2cc5844ae
Wenyue Feng <wenyue.feng@mail.utoronto.ca>
2018-07-05 01:41:03 -0400

    added getter and setter for points and type and completed methods.

--- phase1/src/TransitLine.java
+++ phase1/src/TransitLine.java
@@ -1,19 +1,54 @@
 import java.util.ArrayList;
 
 public class TransitLine {
+
   private ArrayList<String> points;
   private String type;
 
-  TransitLine(ArrayList<String> points, String type) {
+  public TransitLine(ArrayList<String> points, String type) {
     this.points = points;
     this.type = type;
   }
 
-  ArrayList<String> getPoints() {
+  public void setPoints(ArrayList<String> points) {
+    this.points = points;
+  }
+
+  public ArrayList<String> getPoints() {
     return this.points;
   }
 
-  void changeName(String oldName, String newName) {}
+  public void setType(String type) {
+    this.type = type;
+  }
 
-  void addPoint() {}
+  public String getType() {
+    return this.type;
+  }
+
+  public void changeName(String oldName, String newName) {
+    if (!points.isEmpty()) {
+      for (int i = 0; i < points.size(); i++) {
+        if (points.get(i).equals(oldName)) {
+          points.set(i, newName);
+        }
+      }
+    }
+  }
+
+  public void addPoint(String beforenew, String newpoint, String afternew) {
+    if (beforenew.equals("")) {
+      points.add(0, newpoint);
+    } else {
+      if (afternew.equals("")) {
+        points.add(newpoint);
+      } else {
+        for (int i = 0; i < points.size(); i++) {
+          if (points.get(i).equals(beforenew)) {
+            points.add(i + 1, newpoint);
+          }
+        }
+      }
+    }
+  }
 }
