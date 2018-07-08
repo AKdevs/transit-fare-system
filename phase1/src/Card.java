570e029279909ddd666f4b08f64593c9c15f22c0
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-07-08 01:36:06 -0400

    Implemented method 'addTripSegment' in Card.java to deal with cases of CardHolders having continuous trips.

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -1,3 +1,4 @@
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +10,7 @@ public class Card {
   private CardHolder owner;
   private String status;
   private double currentFares;
-  private HashMap<String, ArrayList<ArrayList<TripSegment>>> trips;// key is date
+  private HashMap<String, ArrayList<ArrayList<TripSegment>>> trips; // key is date
   private ArrayList<ArrayList> mostRecentTrips; // [completeTrio1, completeTrip2, completeTrip3]
   private HashMap<String, Double> totalFares; // key-value pair, past 12 months
   private int currentTime;
@@ -72,21 +73,35 @@ public class Card {
     return this.totalFares;
   }
 
-  CardHolder getOwner() { return this.owner; } // do we print out this?
+  CardHolder getOwner() {
+    return this.owner;
+  } // do we print out this?
 
-  void setOwner(CardHolder owner) { this.owner = owner; }
+  void setOwner(CardHolder owner) {
+    this.owner = owner;
+  }
 
-  String getStatus() { return this.status; }
+  String getStatus() {
+    return this.status;
+  }
 
-  double getCurrentFares() { return this.currentFares; }
+  double getCurrentFares() {
+    return this.currentFares;
+  }
 
-  void addCurrentFares(double fares) { this.currentFares += fares; }
+  void addCurrentFares(double fares) {
+    this.currentFares += fares;
+  }
 
-  void deductCurrentFares(double fares) { this.currentFares -= fares; }
+  void deductCurrentFares(double fares) {
+    this.currentFares -= fares;
+  }
 
   void updateTime(int time) {}
 
-  boolean equals(Card other) { return this.cardNumber == other.getCardNumber(); }
+  boolean equals(Card other) {
+    return this.cardNumber == other.getCardNumber();
+  }
 
   boolean isEntryAllowed() {
     if ((this.balance > 0) && (this.status.equals("activated"))) {
@@ -99,30 +114,98 @@ public class Card {
   }
 
   void addTripSegment(TripSegment tripSegment) {
-      if (this.lastTripSegment == null) {
-          String date = tripSegment.getEnterDate();
-          ArrayList<TripSegment> firstCompleteTrip = new ArrayList<>();
-          firstCompleteTrip.add(tripSegment);
-          ArrayList<ArrayList<TripSegment>> firstDayTrips = new ArrayList<>();
-          firstDayTrips.add(firstCompleteTrip);
-          this.trips.put(date, firstDayTrips);
-          this.lastTripSegment = tripSegment;
-          this.lastCompleteTrip = firstCompleteTrip;
-          //deduct fares from card balance
-          this.deductBalance(tripSegment.getSegmentFares());
-          //add currentFares
-          this.addCurrentFares(tripSegment.getSegmentFares());
-          //add fares to totalFares
-          for (Map.Entry month : this.totalFares.entrySet()) {
-              if (month.equals(tripSegment.getMonth())) {
-                  Double tf = (Double) month.getValue();
-                  tf += tripSegment.getSegmentFares();
+    if (this.lastTripSegment == null) {
+      String date = tripSegment.getEnterDate();
+      ArrayList<TripSegment> firstCompleteTrip = new ArrayList<>();
+      firstCompleteTrip.add(tripSegment);
+      ArrayList<ArrayList<TripSegment>> firstDayTrips = new ArrayList<>();
+      firstDayTrips.add(firstCompleteTrip);
+      this.trips.put(date, firstDayTrips);
+      this.lastTripSegment = tripSegment;
+      this.lastCompleteTrip = firstCompleteTrip;
+      // deduct fares from card balance
+      this.deductBalance(tripSegment.getSegmentFares());
+      // add currentFares
+      this.addCurrentFares(tripSegment.getSegmentFares());
+      // add fares to totalFares
+      for (Map.Entry month : this.totalFares.entrySet()) {
+        if (month.equals(tripSegment.getMonth())) {
+          Double tf = (Double) month.getValue();
+          tf += tripSegment.getSegmentFares();
+        }
+      }
+      // add fares to allFares
+      TransitSystem.addAllFares(tripSegment.getSegmentFares());
+    } // if tripSegment and lastTripSegment can form a complete Trip
+    else if (lastTripSegment.getExitSpot().equals(tripSegment.getEnterSpot())) {
+      for (Map.Entry date : this.trips.entrySet()) {
+        if (date.equals(tripSegment.getExitDate())) {
+          ArrayList<ArrayList<TripSegment>> dayTrips =
+              (ArrayList<ArrayList<TripSegment>>) date.getValue();
+          for (ArrayList<TripSegment> ct : dayTrips) {
+            if (ct.equals(this.lastCompleteTrip)) {
+              ct.add(tripSegment);
+              this.lastCompleteTrip.add(tripSegment);
+              this.lastTripSegment = tripSegment;
+              // check whether currentFares + this trip fare > 6
+              if (this.currentFares + tripSegment.getSegmentFares() >= 6) {
+                double difference = 6 - this.currentFares;
+                this.currentFares = 6;
+                // deduct fares from card balance
+                this.deductBalance(difference);
+                // add fares to totalFares
+                for (Map.Entry month : this.totalFares.entrySet()) {
+                  if (month.equals(tripSegment.getMonth())) {
+                    Double tf = (Double) month.getValue();
+                    tf += difference;
                   }
+                }
+                // add fares to allFares
+                TransitSystem.addAllFares(difference);
+              } else {
+                // deduct fares from card balance
+                this.deductBalance(tripSegment.getSegmentFares());
+                // add currentFares
+                this.addCurrentFares(tripSegment.getSegmentFares());
+                // add fares to totalFares
+                for (Map.Entry month : this.totalFares.entrySet()) {
+                  if (month.equals(tripSegment.getMonth())) {
+                    Double tf = (Double) month.getValue();
+                    tf += tripSegment.getSegmentFares();
+                  }
+                }
+                // add fares to allFares
+                TransitSystem.addAllFares(tripSegment.getSegmentFares());
+              }
+            }
           }
-          //add fares to allFares
-          TransitSystem.addAllFares(tripSegment.getSegmentFares());
-      }// if tripSegment and lastTripSegment can form a complete Trip
-      else if (lastTripSegment.getExitSpot().equals(tripSegment.getEnterSpot())) {}
-      // if tripSegment is the start of a new complete trip
+        }
+      }
+    }
+    // if tripSegment is the start of a new complete trip
+    else if (!lastTripSegment.getExitSpot().equals(tripSegment.getEnterSpot())) {
+      for (Map.Entry date : this.trips.entrySet()) {
+        if (date.equals(tripSegment.getExitDate())) {
+          ArrayList<ArrayList<TripSegment>> dayTrips =
+              (ArrayList<ArrayList<TripSegment>>) date.getValue();
+          ArrayList<TripSegment> newCompleteTrip = new ArrayList<>();
+          newCompleteTrip.add(tripSegment);
+          dayTrips.add(newCompleteTrip);
+            // deduct fares from card balance
+            this.deductBalance(tripSegment.getSegmentFares());
+            // add currentFares
+            this.addCurrentFares(tripSegment.getSegmentFares());
+            // add fares to totalFares
+            for (Map.Entry month : this.totalFares.entrySet()) {
+                if (month.equals(tripSegment.getMonth())) {
+                    Double tf = (Double) month.getValue();
+                    tf += tripSegment.getSegmentFares();
+                }
+            }
+            // add fares to allFares
+            TransitSystem.addAllFares(tripSegment.getSegmentFares());
+        }
+      }
+    }
   }
 }
