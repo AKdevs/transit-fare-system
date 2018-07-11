15c1e5e1d84de1958b1d318bc48efb8b7f67b189
Yang Chen <yangadam.chen@mail.utoronto.ca>
2018-07-10 22:05:11 -0400

    Added Doc comments.

--- phase1/src/AdminUser.java
+++ phase1/src/AdminUser.java
@@ -1,6 +1,14 @@
+/**
+ *
+ * AdminUser is used by employee of the Transit System, who can get report to do cost analysis.
+ *
+ */
+
+
 public class AdminUser extends UserAccount {
 
   private static int nextAccountNum = 20000001;
+  /* Account Number for AdminUser starts at 20000001 to distinguish with other account/card numbers.*/
 
   public AdminUser(String name, String email) {
     super(name, email);
@@ -8,6 +16,11 @@ public class AdminUser extends UserAccount {
     nextAccountNum += 1;
   }
 
+  /**
+   * Prints out total amount of fare collected for date and total number of
+   * bus stops/subway stations reached by passengers on that date.
+   * @param date
+   */
   public static void getDailyReport(String date) {
     System.out.println(TransitSystem.getDailyFares(date));
     System.out.println(TransitSystem.getDailyStation(date));
