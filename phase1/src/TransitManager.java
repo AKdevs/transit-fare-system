3a919bf9a915b5bc5d1c722d9ec6e2b860074754
Wenyue Feng <wenyue.feng@mail.utoronto.ca>
2018-07-11 02:22:14 -0400

    Added Javadoc

--- phase1/src/TransitManager.java
+++ phase1/src/TransitManager.java
@@ -2,11 +2,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/**
+ * Manage the transit system.
+ */
 public class TransitManager extends TransitSystem {
+
+  /**
+   * Constructs a transit system manager.
+   */
   TransitManager() {
     defaultSetup();
   }
 
+  /**
+   * Sets up transit lines in transit system.
+   */
   private void defaultSetup() {
     ArrayList<String> line1Stations =
         new ArrayList<>(
@@ -20,10 +30,21 @@ public class TransitManager extends TransitSystem {
     addTransitLine(route1.getId(), route1);
   }
 
+  /**
+   * Creates a new transit line in the transit system.
+   * @param points stations or stops in the new transit line
+   * @param type type of the new transit line
+   * @return the new transit line
+   */
   public TransitLine createTransitLine(ArrayList<String> points, String type) {
     return new TransitLine(points, type, "line1");
   }
 
+  /**
+   * Adds the new transit line to the transit system
+   * @param name id of the new transit line
+   * @param line new transit line
+   */
   public void addTransitLine(String name, TransitLine line) {
     transitLines.put(name, line);
   }
