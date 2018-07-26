9cfbf07653addfdffeebc654ff8485edc272db81
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2018-07-26 17:38:06 -0400

    Changed file structure for javafx.

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 534, 400));
         primaryStage.show();
---
+++ phase2/src/controllers/LoginController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class LoginController {
+    Button forgotPassBtn;
+    public void forgotPasswordButton(ActionEvent event) {
+        System.out.println("Hello World");
+    }
+}
