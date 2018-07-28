a6b1ecabedd4efba91d35a779c9965243c4a0388
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2018-07-28 11:28:23 -0400

    Added new controllers, updated existing ones.

--- phase2/src/controllers/LoginController.java
+++ phase2/src/controllers/LoginController.java
@@ -1,11 +1,23 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LoginController {
-    Button forgotPassBtn;
-    public void forgotPasswordButton(ActionEvent event) {
-        System.out.println("Hello World");
+    Button button = new Button();
+
+    @FXML Button forgotPassbtn;
+    public void forgotPassBtn(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/PasswodRecovery.fxml"));
+        Parent root1 = (Parent) loader.load();
+        Stage recoveryWindow = new Stage();
+        Scene scene = new Scene(root1);
+        recoveryWindow.setScene(scene);
+        recoveryWindow.show();
     }
 }
---
+++ phase2/src/controllers/PasswordRecoveryController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+public class PasswordRecoveryController {
+    public void display() {
+
+    }
+}
