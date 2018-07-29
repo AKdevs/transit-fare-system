794591c90bbe20fbd43745c8e19b21d10d81d208
Wenyue Feng <wenyue.feng@mail.utoronto.ca>
2018-07-29 14:34:25 -0400

    Let CardManager implement Serializable.

--- phase2/src/CardManager.java
+++ phase2/src/CardManager.java
@@ -3,9 +3,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class CardManager {
+public class CardManager implements Serializable {
   /** Keeps a track of all cards in the system. */
   private ArrayList<Card> cards;
 
