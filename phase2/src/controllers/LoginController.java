59ed9979ed93276a8750f8c37cd28cc013cb7a8a
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2018-07-30 11:07:02 -0400

    Added textfield as variable.

--- phase2/src/controllers/LoginController.java
+++ phase2/src/controllers/LoginController.java
@@ -1,13 +1,19 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
+    @FXML
+    private TextField cardTextField;
+
+
     public void showRecovery(ActionEvent event) throws Exception {
         Parent rootParent = FXMLLoader.load(getClass().getResource("../view/PasswordRecovery.fxml"));
         Scene rootScene = new Scene(rootParent);
@@ -16,4 +22,6 @@ public class LoginController {
         window.setScene(rootScene);
         window.show();
     }
+
+
 }
