c3757aad61da642462c3f73dede7bd351b324194
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-07-30 15:19:52 -0400

    Fixed the bug in Main.java.

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -17,9 +17,4 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
