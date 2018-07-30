95d78731e82d435d4b869982b0835122cc8f4485
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-07-30 17:00:36 -0400

    Fixed choicebox in CardHolder window.

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    /*
     @Override
     public void start(Stage primaryStage) throws Exception{
         TransitSystem mainSystem = new TransitSystem();
@@ -17,19 +17,19 @@ public class Main extends Application {
         Scene scene = new Scene(root, 534, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
+    }*/
 
-    /*
+
     @Override
     public void start(Stage stage) throws Exception{
         TransitSystem mainSystem = new TransitSystem();
-        Parent root = FXMLLoader.load(getClass().getResource("view/CardHolderWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/CardHolder.fxml"));
 
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
         stage.show();
-    }*/
+    }
 
     public static void main(String[] args) {
         launch(args);
