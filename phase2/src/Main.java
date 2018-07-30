ca5d1fb0969f633d90d46ab6d47cca1be87607e6
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-07-30 14:40:09 -0400

    Modified Main.java so we can run the program.

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Card.fxml"));
 
         Scene scene = new Scene(root);
 
