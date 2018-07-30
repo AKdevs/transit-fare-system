0049c7756a9879fb0049ff227efa5b64cff8d9bb
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-07-30 15:40:05 -0400

    Added extra code in Main.java so we can open the windows without going through the Login Window.

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    /*
     @Override
     public void start(Stage primaryStage) throws Exception{
         TransitSystem mainSystem = new TransitSystem();
@@ -16,5 +17,20 @@ public class Main extends Application {
         Scene scene = new Scene(root, 534, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    */
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
