94844cf977e455b95eafe0a0cce3281fba5ae9ea
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-08-01 16:19:30 -0400

    Fixed backToCard button in travelSimulation window.

--- phase2/src/TravelSimulationController.java
+++ phase2/src/TravelSimulationController.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.URL;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import java.util.ResourceBundle;
@@ -8,31 +9,44 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
 public class TravelSimulationController extends Controller {
-    /*
+
     @FXML private Button tapIn;
     @FXML private Button tapOut;
     @FXML private Button backToCard;
-    */
+
+    @FXML private Label cardNumber;
+
 
     public void initialize(URL url, ResourceBundle rb) {
-        /*this.tapIn.setText("Tap In");
-        this.tapOut.setText("Tap Out");
-        this.backToCard.setText("Back To Card");*/
+
+    }
+
+    void initialTravelSimulationInfo(String cardNum) {
+        cardNumber.setText(cardNum);
     }
 
     //change scene method
     public void backToCardButtonPushed(ActionEvent event) throws IOException {
-        // new Scene
-        Parent cardParent = FXMLLoader.load(getClass().getResource("view/Card.fxml"));
-        Scene cardScene = new Scene(cardParent);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("view/Card.fxml"));
+        Parent cardHolderParent = loader.load();
+
+        Scene cardHolderScene = new Scene(cardHolderParent);
+
+        // read user input and set value in CardHolder window
+        CardController ct = loader.getController();
+        ct.storeState(system);
+        ct.initialCardInfo(cardNumber.getText());
 
         // get the Stage info
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setScene(cardScene);
+        window.setScene(cardHolderScene);
         window.show();
     }
 }
