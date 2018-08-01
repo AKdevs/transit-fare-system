e5edbc91eca2b166653f406ab39a4bdf514b6f97
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2018-08-01 16:58:06 -0400

    Added text field verification (for empty fields) in CreateAccountController.

--- phase2/src/CreateAccountController.java
+++ phase2/src/CreateAccountController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -12,22 +13,30 @@ import java.io.IOException;
 
 public class CreateAccountController extends Controller {
 
-
+    @FXML private Label requiredInfoLabel;
     @FXML private TextField name;
     @FXML private TextField email;
     @FXML private TextField password;
 
-    public void createAccount(ActionEvent event) throws IOException {
+    public void createAccountButtonPushed(ActionEvent event) throws IOException {
         String currentName = name.getText();
         String currentEmail = email.getText();
         String currentPassword = password.getText();
 
-        system.getAccountManager().createCardHolderAccount(currentName,
-                currentEmail, currentPassword);
-        showLogin(event);
+        if (currentName.equals("") || currentEmail.equals("") ||
+                currentPassword.equals("")) {
+            requiredInfoLabel.setText("* Please fill all required fields");
+        } else {
+            system.getAccountManager().createCardHolderAccount(currentName,
+                    currentEmail, currentPassword);
+            changeWindow(event,"view/Login.fxml" );
+        }
+
     }
 
-    public void showLogin(ActionEvent event) throws IOException {
-        changeWindow(event,"view/Login.fxml" );
+
+
+    public void cancelButtonPushed(ActionEvent event) throws IOException {
+        changeWindowToHome(event);
     }
 }
--- phase2/src/PasswordRecoveryController.java
+++ phase2/src/PasswordRecoveryController.java
@@ -1,10 +1,4 @@
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class PasswordRecoveryController extends Controller {
