0702178492dd9247cedbeb61d7d13f2bcd2c7486
Yang Chen <yangadam.chen@utoronto.ca>
2018-08-01 00:28:54 -0400

    Added Daily Report and Transit Scheduling sections in AdminUser view. Populated Transit Line list in AdminUserController.

--- phase2/src/AdminUserController.java
+++ phase2/src/AdminUserController.java
@@ -9,17 +9,31 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.io.IOException;
+import java.util.Set;
 
 
-public class AdminUserController extends Controller {
-
+public class AdminUserController extends Controller implements Initializable {
+        @FXML private Button returnToMainButton;
         @FXML private Button powerOnButton;
         @FXML private Button powerOffButton;
         @FXML private Label powerResult;
+        @FXML private TextField dailyReportDate;
+        @FXML private Button runDailyReportButton;
+        @FXML private ComboBox<String> transitLinesList;
+        @FXML private TextField transitSchedulingDate;
+        @FXML private TextField transitNumOfTrips;
+        @FXML private Button setSchedulingButton;
+        @FXML private Label schedulingResult;
+
 
         TransitSystem theTransitSystem = TransitSystem.getInstance();
+
+        public void returnToMain(ActionEvent event) throws IOException {
+            changeWindow(event, "view/login.fxml");
+        }
 
 
         public void powerOnSystem(ActionEvent event) throws IOException {
@@ -34,10 +48,15 @@ public class AdminUserController extends Controller {
             powerResult.setText("The Transit System has been shut down.");
         }
 
-        /**@Override
+        @Override
         public void initialize(URL url, ResourceBundle rb) {
 
-        }*/
+            Set<String> transitLines =  theTransitSystem.getTransitManager().getTransitLines().keySet();
+            for (String line : transitLines) {
+                transitLinesList.getItems().add(line);
+            }
+
+        }
 
 }
 
