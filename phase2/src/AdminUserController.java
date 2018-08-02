92672566ca969565015bf70e80f192d0d2e78cf3
Yang Chen <yangadam.chen@utoronto.ca>
2018-08-01 20:25:47 -0400

    Updated to show system operating status in System Management section.

--- phase2/src/AdminUserController.java
+++ phase2/src/AdminUserController.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
@@ -19,7 +20,7 @@ public class AdminUserController extends Controller implements Initializable {
         @FXML private Button returnToMainButton;
         @FXML private Button powerOnButton;
         @FXML private Button powerOffButton;
-        @FXML private Label powerResult;
+        @FXML private Label systemStatusLabel;
         @FXML private TextField dailyReportDate;
         @FXML private Button runDailyReportButton;
         @FXML private ComboBox<String> transitLinesList;
@@ -39,19 +40,37 @@ public class AdminUserController extends Controller implements Initializable {
         public void powerOnSystem(ActionEvent event) throws IOException {
             // theTransitSystem.powerOnSystem();
             system.powerOnSystem();
-            powerResult.setText("The Transit System is now operating.");
+            showSystemStatus();
+
+            // powerResult.setText("The Transit System is now operating.");
             //System.out.println(theTransitSystem.getOperatingStatus());
         }
 
         public void powerOffSystem(ActionEvent event) throws IOException{
-            system.powerOnSystem();
+            system.powerOffSystem();
+            showSystemStatus();
             //theTransitSystem.powerOffSystem();
             //System.out.println(theTransitSystem.getOperatingStatus());
-            powerResult.setText("The Transit System has been shut down.");
+            //powerResult.setText("The Transit System has been shut down.");
+        }
+
+        public void showSystemStatus() {
+            if (system.getOperatingStatus().equals("on")){
+                systemStatusLabel.setText("on");
+            systemStatusLabel.setTextFill(Color.GREEN);
+        }
+            if (system.getOperatingStatus().equals("off")) {
+                systemStatusLabel.setText("off");
+                systemStatusLabel.setTextFill(Color.ORANGE);
+            }
+
         }
 
         @Override
         public void initialize(URL url, ResourceBundle rb) {
+
+            //String systemStatus = system.getOperatingStatus();
+            showSystemStatus();
 
             // Set<String> transitLines =  theTransitSystem.getTransitManager().getTransitLines().keySet();
             Set<String> transitLines =  system.getTransitManager().getTransitLines().keySet();
