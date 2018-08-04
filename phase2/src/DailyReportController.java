797c93f94bae9117c5f75f16ee93e88ce542bc96
Yang Chen <yangadam.chen@utoronto.ca>
2018-08-04 18:04:28 -0400

    Designed Daily Report window.

---
+++ phase2/src/DailyReportController.java
@@ -0,0 +1,36 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.util.StringConverter;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.io.IOException;
+import java.util.Set;
+import java.text.DateFormat;
+
+
+public class DailyReportController extends Controller implements Initializable {
+
+    @FXML private Label dateLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+
+    }
+
+
+}
