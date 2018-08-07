73d7786970b4603c7fdf80276ed6df1b99661ba5
Atharva Karandikar <ak.karandikar@mail.utoronto.ca>
2018-08-07 13:34:08 -0400

    Changed file path of serializedobjects.ser

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        String path="serializedobjects.ser";
+        String path="./serializedobjects.ser";
         TransitSystem mainSystem = new TransitSystem(path);
         mainSystem.readFromFile(path);
         FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("view/Login.fxml"));
