b312e80999ab1eee1e4264cfd21a98e936f45de2
Jingjing Zhan <isabel.zhan@mail.utoronto.ca>
2018-08-07 13:25:26 -0400

    Deleted unnecessary code in Main.java.

--- phase2/src/Main.java
+++ phase2/src/Main.java
@@ -31,39 +31,6 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
-    /*
-    @Override
-    public void start(Stage stage) throws Exception{
-
-        TransitSystem mainSystem = new TransitSystem();
-
-        mainSystem.getAccountManager().createAdminAccount("sysadmin", "admin@gmail.com", "sysadmin");
-        mainSystem.getAccountManager().createCardHolderAccount("Isabel", "isabel@gmail.com", "123456");
-        mainSystem.getAccountManager().createCardHolderAccount("Amy", "amy@gmail.com", "123456");
-        mainSystem.getAccountManager().createCardHolderAccount("Yang", "yang@gmail.com", "123456");
-
-        mainSystem.getCardManager().createCard();
-        mainSystem.getCardManager().createCard();
-        mainSystem.getCardManager().createCard();
-
-        Card c1 = mainSystem.getCardManager().findCard("30000001");
-        Card c2 = mainSystem.getCardManager().findCard("30000002");
-        Card c3 = mainSystem.getCardManager().findCard("30000003");
-
-        ((CardHolder)mainSystem.getAccountManager().findUserAccount("10000001")).linkCard(c1);
-        ((CardHolder)mainSystem.getAccountManager().findUserAccount("10000001")).linkCard(c2);
-        ((CardHolder)mainSystem.getAccountManager().findUserAccount("10000002")).linkCard(c3);
-
-        Parent root = FXMLLoader.load(getClass().getResource("view/CardHolder.fxml"));
-
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.show();
-    }*/
-
-
     public static void main(String[] args) {
         launch(args);
     }
